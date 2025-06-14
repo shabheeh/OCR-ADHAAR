9b108cd839db7de4de1cc931b41c60c87dc02983
shabeeh <shabeeh573@gmail.com>
2025-06-14 15:00:26 +0530

    added cors for router

--- server/src/routes/adhaar.routes.ts
+++ server/src/routes/adhaar.routes.ts
@@ -3,6 +3,7 @@ import { container } from "../configs/inversify.config";
 import { IAdhaarController } from "../controllers/adhaar.controller.interface";
 import TYPES from "../shared/types/inversifyjs.types";
 import upload from "../configs/multer.config";
+import cors from "cors";
 
 const router = Router();
 
@@ -10,6 +11,9 @@ const controller = container.get<IAdhaarController>(TYPES.AdhaarController);
 
 router.post(
   "/adhaars",
+
+  cors({ origin: process.env.CLIENT_URL, credentials: true }),
+
   upload.fields([
     { name: "front", maxCount: 1 },
     { name: "back", maxCount: 1 },
